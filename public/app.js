80e5d066500fc7112bfc021b93f77fe2199ecdb6
klhelker <57188023+klhelker@users.noreply.github.com>
2020-04-13 22:42:24 -0500

    get route

--- public/app.js
+++ public/app.js
@@ -4,3 +4,10 @@ $("#scrapeBtn").on("click",function() {
       });
 });
 
+$.getJSON("/articles", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#articles").append("<p data-id='" + data[i].title + "'>" + data[i].summary + "<br />" + data[i].link + "</p>");
+  }
+});
